7e14a7698b81d11f06245e6024fc3e818cf928ce
deamsy <gerritegberink3@gmail.com>
2021-03-08 19:57:09 +0100

    just get rid of it

--- src/main/kotlin/dev/systemless/systemless/bus/EventBus.kt
+++
@@ -1,8 +0,0 @@
-package dev.systemless.systemless.bus
-
-import me.kbrewster.eventbus.eventbus
-import me.kbrewster.eventbus.invokers.LMFInvoker
-
-val eventBus = eventbus {
-    invoker { LMFInvoker() }
-}
